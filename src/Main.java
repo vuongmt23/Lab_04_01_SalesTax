bea746e90005bfa5c8797823e27cf1df3c9b9b04
Minh Vuong <vuongmt@mail.uc.edu>
2024-09-23 10:40:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double purchasePrice = 100.00; // Replace with any value
+        double salesTaxRate = 0.05;
+        double salesTax = purchasePrice * salesTaxRate;
+
+        System.out.println("The purchase price is: $" + purchasePrice);
+        System.out.println("The sales tax at 5% is: $" + salesTax);
+    }
+}
